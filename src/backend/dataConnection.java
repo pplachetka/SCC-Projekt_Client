27d2f3cd2807f3b316b0344c3b5173dc64e18fe1
Paul <paul_richard.plachetka@mailbox.tu-dresden.de>
2019-11-25 09:53:54 +0100

    Wechsel von Get zu Post in login()

--- src/backend/dataConnection.java
+++ src/backend/dataConnection.java
@@ -1,10 +1,13 @@
 package backend;
 
+import com.sun.xml.internal.ws.api.pipe.ContentType;
+
 import javax.net.ssl.HttpsURLConnection;
 import javax.net.ssl.SSLPeerUnverifiedException;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.net.MalformedURLException;
 import java.net.ProtocolException;
 import java.net.URL;
@@ -20,13 +23,19 @@ public class dataConnection {
     public String loginUser(String username, String password) throws MalformedURLException {
         StringBuilder token = new StringBuilder();
 
-        URL idCheckURL = new URL("login/loginuser/"+ Base64.getEncoder().encodeToString((username+":"+password).getBytes()));
+        URL idCheckURL = new URL("login/loginuser");
 
         try {
             HttpsURLConnection con = (HttpsURLConnection) idCheckURL.openConnection();
 
-            con.setRequestMethod("GET");
+            con.setRequestMethod("POST");
+            con.setRequestProperty("ContentType", "text/plain");
+            con.setRequestProperty("Accept", "text/plain");
             con.setDoOutput(true);
+
+            OutputStream os = con.getOutputStream();
+            byte[] streamline = Base64.getEncoder().encodeToString((username+":"+password).getBytes()).getBytes();
+            os.write(streamline);
 
             BufferedReader br = new BufferedReader(new InputStreamReader(con.getInputStream()));
 
